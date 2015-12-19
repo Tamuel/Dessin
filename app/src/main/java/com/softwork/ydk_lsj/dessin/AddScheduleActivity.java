6d3af3096770518e898f3fa0aa6b039c1fb46e22
Seung-jin <fewfree12@naver.com>
2015-12-19 19:52:19 +0900

    약간 수정

--- app/src/main/java/com/softwork/ydk_lsj/dessin/AddScheduleActivity.java
+++ app/src/main/java/com/softwork/ydk_lsj/dessin/AddScheduleActivity.java
@@ -15,9 +15,12 @@ import java.util.ArrayList;
 
 public class AddScheduleActivity extends AppCompatActivity {
     private ArrayList<String> yearArray, monthArray, dayArray;
-    private Spinner yearSpinner, monthSpinner, daySpinner;
+    private Spinner startYearSpinner, startMonthSpinner, startDaySpinner;
+    private Spinner endYearSpinner, endMonthSpinner, endDaySpinner;
+    private Spinner addYearSpinner, addMonthSpinner, addDaySpinner;
     private ArrayAdapter<String> yearAdapter, monthAdapter, dayAdapter;
-    private String year, month, day;
+    private String startyear, startmonth, startday;
+    private String endyear, endmonth, endday
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,66 +33,19 @@ public class AddScheduleActivity extends AppCompatActivity {
         dayArray = new ArrayList<String> ();
         setDateArray();
 
-        //spinner 설정
-        yearSpinner = (Spinner)findViewById(R.id.year_spinner);
-        monthSpinner = (Spinner)findViewById(R.id.month_spinner);
-        daySpinner = (Spinner)findViewById(R.id.day_spinner);
+        //startSpinner 설정
+        startYearSpinner = (Spinner)findViewById(R.id.start_year_spinner);
+        startMonthSpinner = (Spinner)findViewById(R.id.start_month_spinner);
+        startDaySpinner = (Spinner)findViewById(R.id.start_day_spinner);
+        setSpinner(startYearSpinner, startMonthSpinner, startDaySpinner, startyear, startmonth, startday);
 
-        yearAdapter = new ArrayAdapter<String>(this,
-                R.layout.support_simple_spinner_dropdown_item, yearArray);
-        yearSpinner.setAdapter(yearAdapter);
-        yearSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                year = parent.getItemAtPosition(position).toString();
-            }
+        //endSpinner 설정
+        endYearSpinner = (Spinner)findViewById(R.id.end_year_spinner);
+        endMonthSpinner = (Spinner)findViewById(R.id.end_month_spinner);
+        endDaySpinner = (Spinner)findViewById(R.id.end_day_spinner);
+        setSpinner(endYearSpinner, endMonthSpinner, endDaySpinner, endyear, endmonth, endday);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
 
-        monthAdapter = new ArrayAdapter<String>(this,
-                R.layout.support_simple_spinner_dropdown_item, monthArray);
-        monthSpinner.setAdapter(monthAdapter);
-        monthSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                month = parent.getItemAtPosition(position).toString();
-                //매 월마다 날짜가 다르기 때문에 월별로 설정
-                dayArray.clear();
-                if (month.equals("1") || month.equals( "3") || month.equals("5") || month.equals("7")
-                        || month.equals("8") || month.equals("10") || month.equals("12"))
-                    for (int i = 0; i < 31; i++)
-                        dayArray.add(String.valueOf(i + 1));
-                else if (month.equals("4") || month.equals("6") || month.equals("9") || month.equals("11"))
-                    for (int i = 0; i < 30; i++)
-                        dayArray.add(String.valueOf(i + 1));
-                else
-                    for (int i = 0; i < 28; i++)
-                        dayArray.add(String.valueOf(i + 1));
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        dayAdapter = new ArrayAdapter<String>(this,
-                R.layout.support_simple_spinner_dropdown_item, dayArray);
-        daySpinner.setAdapter(dayAdapter);
-        daySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                day = parent.getItemAtPosition(position).toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
     }
 
     @Override
@@ -119,5 +75,64 @@ public class AddScheduleActivity extends AppCompatActivity {
             yearArray.add(String.valueOf(i));
         for(int i=1; i<=12; i++)
             monthArray.add(String.valueOf(i));
+    }
+
+    //Spinner 설정 메소드
+    public void setSpinner(Spinner yearSpinner, Spinner monthSpinner, Spinner daySpinner) {
+        yearAdapter = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, yearArray);
+        yearSpinner.setAdapter(yearAdapter);
+        yearSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                startyear = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        monthAdapter = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, monthArray);
+        monthSpinner.setAdapter(monthAdapter);
+        monthSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                startmonth = parent.getItemAtPosition(position).toString();
+                //매 월마다 날짜가 다르기 때문에 월별로 설정
+                dayArray.clear();
+                if (startmonth.equals("1") || startmonth.equals( "3") || startmonth.equals("5") || startmonth.equals("7")
+                        || startmonth.equals("8") || startmonth.equals("10") || startmonth.equals("12"))
+                    for (int i = 0; i < 31; i++)
+                        dayArray.add(String.valueOf(i + 1));
+                else if (startmonth.equals("4") || startmonth.equals("6") || startmonth.equals("9") || startmonth.equals("11"))
+                    for (int i = 0; i < 30; i++)
+                        dayArray.add(String.valueOf(i + 1));
+                else
+                    for (int i = 0; i < 28; i++)
+                        dayArray.add(String.valueOf(i + 1));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        dayAdapter = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item, dayArray);
+        daySpinner.setAdapter(dayAdapter);
+        daySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                startday = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
